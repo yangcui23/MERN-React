d6172244c70638d915d89f9654f05a456aa372ec
Yang Cui <123989119+yangcui23@users.noreply.github.com>
2023-04-03 13:17:08 -0600

    Add files via upload

---
+++ first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do :</h2>
+      <ul>
+        <li>Wake Up Early</li>
+        <li>Workout everyday</li>
+        <li>Work on algorithm</li>
+        <li>be more efficient</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
