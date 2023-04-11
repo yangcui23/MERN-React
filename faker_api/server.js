15b29bd7200a8920818cf06dda23f8c5f4ea79dd
Yang Cui <123989119+yangcui23@users.noreply.github.com>
2023-04-10 22:32:04 -0600

    Add files via upload

---
+++ faker_api/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const { faker } = require('@faker-js/faker');
+
+const createUser = () => {
+  const newFake = {
+    username: faker.internet.userName(),
+    userId: faker.datatype.uuid(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.number()
+  };
+  return newFake;
+};
+
+const createCompany = () => {
+  const newComp = {
+    company: faker.company.name(),
+    companyId: faker.datatype.uuid(),
+    address: {
+      street: faker.address.street(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zipcode: faker.address.zipCode(),
+      country: faker.address.country()
+    }
+  };
+  return newComp
+};
+
+
+app.get("/api/users/new", (req, res) => {
+  res.json(createUser());
+});
+
+
+
+app.get("/api/companies/new", (req, res) => {
+  res.json(createCompany());
+});
+
+// app.get("/api/user/companies", (req, res) => {
+//   res.json([createUser(), createCompany()]);
+// });
+
+app.get("/api/user/company", (req, res) => {
+
+  res.json([createUser(), createCompany()]);
+});
+// this needs to be below the other code blocks
+// app.listen( port, () => console.log(`Listening on port: ${port}`) );
+app.listen(port, () => console.log(`Listening on port: ${port} HUH? `));
